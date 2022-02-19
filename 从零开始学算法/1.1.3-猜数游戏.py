e2685dbe4b361f8946c74875b54128e879c02ec6
vincent-yangcheng <vincent.yangcheng@gmail.com>
2022-02-19 21:49:38 +0800

    1

--- 从零开始学算法/1.1.3-猜数游戏.py
+++ 从零开始学算法/1.1.3-猜数游戏.py
@@ -1,12 +1,11 @@
-#_*_ coding:utf-8 _*_
-
-from sqlalchemy import DDL
-
-
-def bscarch(a, low, high, value):
-    middle = (low + high) // 2
-
-
-
-    DDL
-22222222222
+#!/usr/bin/env python3
+ 
+n = 100
+ 
+sum = 0
+counter = 1
+while counter <= n:
+    sum = sum + counter
+    counter += 1
+ 
+print("1 到 %d 之和为: %d" % (n,sum))
