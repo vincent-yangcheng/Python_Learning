b27e020e3b7edcd18c22c111f6247a4826d16ab0
vincent-yangcheng <vincent.yangcheng@gmail.com>
2024-11-16 21:09:42 +0800

    1

--- Python 100天练习/Pay.py
+++ Python 100天练习/Pay.py
@@ -1,6 +1,17 @@
 # This first line is provided for you
 
-hrs = input("Enter Hours:")
-Rate = 2.75
-Pay = float(hrs) * Rate
-print ("Pay: " "str(Pay)")
+score_inout = input("Enter Score: ")
+score = float(score_input)
+
+if score < 0.0 or score > 1.0:
+    print ("It's wrong input")
+elif score >= 0.9:
+    print ("A")
+elif score >= 0.8:
+    print ("B")
+elif score >= 0.7:
+    print ("C")
+elif score >= 0.6:
+    print ("D")
+elif score >= 0.0:
+    print ("F")
