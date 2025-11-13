86cba4c333439b69713f1d4c87b29e338676d1a7
vincent-yangcheng <vincent.yangcheng@gmail.com>
2025-11-13 10:25:33 +0800

    11

--- Python-100-Day/day01-20/04.运算符/计算圆的周长和面机2.py
+++ Python-100-Day/day01-20/04.运算符/计算圆的周长和面机2.py
@@ -5,9 +5,11 @@ Version: 1.2
 Author: Ethan
 """
 import math
+from re import T
 
 radius = float(input('请输入圆的半径: '))  # 输入: 5.5
 perimeter = 2 * math.pi * radius
 area = math.pi * radius ** 2
 print(f'{perimeter = :.4f}')  # 输出：perimeter = 34.56
 print(f'{area = :.4f}')       # 输出：area = 95.035
+T
